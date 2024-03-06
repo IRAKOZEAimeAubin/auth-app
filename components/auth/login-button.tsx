07134c359b79cc811a72c77f2aeea5e6718706d2
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2024-03-06 11:15:00 +0200

    ninth commit

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { useRouter } from "next/navigation"
+import {useRouter} from 'next/navigation'
+import {Dialog, DialogContent, DialogTrigger} from '@/components/ui/dialog'
+import {LoginForm} from '@/components/auth/login-form'
 
 interface LoginButtonProps {
   children: React.ReactNode
@@ -12,15 +14,22 @@ export const LoginButton = ({
   children,
   mode = 'redirect',
   asChild,
-}: LoginButtonProps ) => {
-    const router = useRouter()
-    
+}: LoginButtonProps) => {
+  const router = useRouter()
+
   const onClick = () => {
     router.push('/auth/login')
   }
 
   if (mode === 'modal') {
-    return <span>TODO: Implement modal</span>
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+        <DialogContent className='p-0 w-auto bg-transparent border-none'>
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    )
   }
 
   return (
--- middleware.ts
+++ middleware.ts
@@ -18,21 +18,30 @@ export default auth((req) => {
   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
 
   if (isApiAuthRoute) {
-    return 
+    return
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
     }
-    return 
+    return
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL('/auth/login', nextUrl))
+    let callbackUrl = nextUrl.pathname
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+
+    return Response.redirect(
+      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+    )
   }
 
-  return 
+  return
 })
 
 export const config = {
